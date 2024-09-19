2893bde9c976e91a5b6fa8fea40c421190701d4f
胡成杭 <huchenghang@huchenghangdeiMac.local>
2024-09-19 16:59:43 +0800

    添加一个 matrix 的例子

---
+++ chapter3/identity_matrix.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "matrix.h"
+
+void identity_matrix(size_t n)
+{
+    matrix<int> M(n, n, 0);
+    // 对角线赋值
+    for (size_t i = 0; i < M.x_size(); ++i)
+        M(i, i) = 1;
+    // 按行列打印该矩阵
+    for (size_t i = 0; i < M.y_size(); ++i)
+    {
+        for (size_t j = 0; j < M.y_size(); ++j)
+            std::cout << M(i, j) << " ";
+        std::cout << std::endl;
+    }
+}
+
+int main()
+{
+    identity_matrix(5);
+    return 0;
+}
